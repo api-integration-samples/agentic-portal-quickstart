362e30efc4a6ea3189268863b12d07f305464f63
tyayers <tyler.ayers@gmail.com>
2025-11-17 12:31:49 +0100

    updated tutorial

--- 3.deploy-service.sh
+++ 3.deploy-service.sh
@@ -1,7 +1,7 @@
 SECONDS=0
 
 gcloud run deploy agentic-portal --source . --project $PROJECT_ID --region $REGION --allow-unauthenticated \
-    --env-vars-file ".env" --service-account "agentic-portal-service@$PROJECT_ID.iam.gserviceaccount.com"
+    --env-vars-file "./.env" --service-account "agentic-portal-service@$PROJECT_ID.iam.gserviceaccount.com"
 
 duration=$SECONDS
 echo "Total deployment finished in $((duration / 60)) minutes and $((duration % 60)) seconds."
